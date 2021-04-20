64c75899d97eaf0c50682d6ea4faf1bfad9632e2
Nathaniel A <ncarmstrong92@gmail.com>
2021-04-20 15:38:16 -0400

    Added mongoDB and models folder in server

---
+++ server/models/mongoose_models.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://n8innate:codesmith@cluster0.n9knr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'solo_project'
+
+  // sets the name of the DB that our collections are part of
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+const Schema = mongoose.Schema;
+
+
+const speciesSchema = new Schema({
+  dev_type: String,
+  // classification: String,
+  // average_height: String,
+  // average_lifespan: String,
+  // hair_colors: String,
+  // skin_colors: String,
+  // eye_colors: String,
+  // language: String,
+  // homeworld: String,
+  // homeworld_id: {
+  //   // type of ObjectId makes this behave like a foreign key referencing the 'planet' collection
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'planet'
+  // }
+});
+
+
+const Species = mongoose.model('species', speciesSchema);
+
+
+module.exports = {
+  Species,
+  // Planet,
+  // Film,
+  // Person
+};
--- webpack.config.js
+++ webpack.config.js
@@ -14,17 +14,50 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    proxy: {
+      '/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+  },
   devtool: 'eval-source-map',
   mode: 'development',
   module: {
     rules: [
         {
-            test: /\.(js)$/, 
-            use: 'babel-loader' 
+          test: /.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env','@babel/preset-react']
+            }
+          },
         },
-        { 
-            test: /\.css$/, 
-            use: ['style-loader', 'css-loader'] 
+        {
+          test: /.(css|scss)$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          
+        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg|ico)$/,
+        use: [
+          {
+            // loads files as base64 encoded data url if image file is less than set limit
+            loader: 'url-loader',
+            options: {
+              // if file is greater than the limit (bytes), file-loader is used as fallback
+              limit: 8192,
+            },
+          },
+        ],
+      
         }
       ]
   },
@@ -32,5 +65,9 @@ module.exports = {
     new HtmlWebpackPlugin({
         template: './client/index.html'
     })
-  ]
+  ],
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
 }
